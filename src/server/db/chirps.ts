9907c8d34a32651cf13b5044665bd8da26440c0c
solgamd <solgamd@gmail.com>
2020-06-04 20:20:52 -0500

    Update DB files w/ id:string and userid: string (to fix bugs)

--- src/server/db/chirps.ts
+++ src/server/db/chirps.ts
@@ -2,15 +2,15 @@ import { Query } from './index';
 
 const all = async () => Query('SELECT users.name, chirps.* FROM chirps JOIN users ON users.id = chirps.userid ORDER BY _created DESC');
 
-const one = async (id: number) => Query('SELECT users.name, chirps.* FROM chirps JOIN users ON users.id = chirps.userid WHERE userid = ?', [id]);
+const one = async (id: string) => Query('SELECT users.name, chirps.* FROM chirps JOIN users ON users.id = chirps.userid WHERE userid = ?', [id]);
 
-const post = async (userid: number, chirp: string, location: string) => {
+const post = async (userid: string, chirp: string, location: string) => {
     return Query('INSERT INTO chirps (userid, chirp, location) value (?)', [userid, chirp,location])
 };
 
-const remove = async (id: number) => Query('DELETE FROM chirps WHERE id = ?', [id]);
+const remove = async (id: string) => Query('DELETE FROM chirps WHERE id = ?', [id]);
 
-const update = async (id: number, chirp: string) => {
+const update = async (id: string, chirp: string) => {
     return Query('UPDATE chirps SET chirp = ? WHERE id = ' + id + '', [chirp])
 };
 
--- src/server/db/mentions.ts
+++ src/server/db/mentions.ts
@@ -1,6 +1,6 @@
 import { Query } from './index';
 
-const getUserMentions = (userid: number) => Query('CALL spGetMentions(?);', [userid]);
+const getUserMentions = (userid: string) => Query('CALL spGetMentions(?);', [userid]);
 
 export default {
     getUserMentions
